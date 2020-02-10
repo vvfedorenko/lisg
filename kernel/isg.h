16298afc31c0b97a2f75d0050ccc89d638b8531e
Vadim Fedoreko <vadimjunk@gmail.com>
2020-02-11 00:55:40 +0300

    isg: move common structures and definition to separate file
    As common structures are used to transfer data from userspace
    to kernel space move them to separate header file to always
    use the same structures in kernel and library

---
+++ kernel/isg.h
@@ -0,0 +1,16 @@
+#ifndef _ISG_H
+#define _ISG_H
+
+#define INIT_SESSION  0x01
+#define INIT_BY_SRC   0x02
+#define INIT_BY_DST   0x04
+
+struct ipt_ISG_info {
+	u_int8_t flags;
+};
+
+struct ipt_ISG_mt_info {
+	u_int8_t service_name[32];
+};
+
+#endif
